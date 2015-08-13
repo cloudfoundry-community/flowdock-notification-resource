666dbfb8da92847911cd4b2d56a0f5db71363a36
Quinn <quintessenceanx@gmail.com>
2015-08-13 18:40:55 -0400

    changed URL to concourse CI

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+#concourse_pipeline_baseurl="http://10.202.75.109/pipelines/
+
 COMMAND="curl -i -X POST -H \"Content-Type: application/json\" -d '${MESSAGE}' ${API_URL}"
 #echo $COMMAND
 eval $COMMAND
