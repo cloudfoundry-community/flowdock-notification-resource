fd64d6dee3040e6f85f307b0351730276d6a520b
Quinn <quintessenceanx@gmail.com>
2015-08-13 18:08:02 -0400

    Added basic thread ID handling

--- README.md
+++ README.md
@@ -3,4 +3,7 @@ Concourse CI resource for sending notifications to Flowdock.
 
 ## Setup
 
-Change the `.flow_token-example` file name to `.flow_token` and then replace the psuedotoken with the flow token that was generated when you set up the source for your developer application.
+* Change `.flow_token-example` to `.flow_token` and replace the psuedotoken with the flow token that was generated when you set up the source for your developer application.
+* Change `.thread_id-example` to `.thread_id`. The thread ID does not need to be changed unless you do not wish to start at 1.
+
+The purpose of the separation is to avoid thread collision where a message is posted to the wrong thread. If a thread ID is reused the original message will not be deleted, the new message will merely be placed in a potentially unrelated thread.
--- test.sh
+++ test.sh
@@ -1,18 +1,21 @@
 concourse_display_name="Concourse Drone [Test]"
 concourse_notify_from_email="test@example.com"
-concourse_msg_threadID=4
+concourse_msg_threadID=$(cat .thread_id)
 concourse_avatar="http://cl.ly/image/3e1h0H3H2s0P/concourse-logo.png"
 concourse_pipeline_url="http://10.202.75.109/pipelines/microbosh-deployments"
 concourse_flow_token=$(cat .flow_token)
 concourse_msg_event="activity" # activity or discussion
 concourse_msg_status_value="Drone Test"
-concourse_msg_status_color="cyan" # using purple to differentiate all test messages from "regular" msgs
+concourse_msg_status_color="purple" # using purple to differentiate all test messages from "regular" msgs
 concourse_pipeline_name="Test Pipeline"
 concourse_msg_title="TESTING WITH TEST SCRIPT"
 concourse_msg_thread_title="Yet Another Test: ${concourse_msg_threadID}"
 concourse_msg_field_label="Status Message"
 concourse_msg_field_value="All tests passed."
 concourse_msg_body="This message is to test the Concourse FlowDrone integration."
+
+echo $(expr ${concourse_msg_threadID} + 1) > .thread_id
+
 
 HEADER="\"flow_token\": \"${concourse_flow_token}\", \"event\": \"${concourse_msg_event}\", \"author\": {\"name\": \"${concourse_display_name}\", \"avatar\": \"${concourse_avatar}\"}"
 MSGINFO="\"title\": \"${concourse_msg_title}\", \"external_thread_id\": \"${concourse_msg_threadID}\""
