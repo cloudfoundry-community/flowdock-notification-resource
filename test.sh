267bf98f27efdfc9444042a2f194afeaabd6ec65
Quinn <quintessenceanx@gmail.com>
2015-08-21 10:33:11 -0400

    Removing need for run.sh; using cred and pipeline yml files for Concourse

--- test.sh
+++ test.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 concourse_display_name="Concourse Drone [Test]"
 concourse_notify_from_email="test@example.com"
-concourse_msg_threadID=$(cat .thread_id)
+concourse_msg_threadID=11000
 concourse_avatar="http://cl.ly/image/3e1h0H3H2s0P/concourse-logo.png"
 concourse_pipeline_url="https://ci.concourse.ci/"
-concourse_flow_token=$(cat .flow_token)
+concourse_flow_token="FLOWTOKEN"
 concourse_msg_event="activity" # activity or discussion
 concourse_msg_status_value="Drone Test"
 concourse_msg_status_color="purple" # using purple to differentiate all test messages from "regular" msgs
@@ -14,9 +16,6 @@ concourse_msg_field_label="Status Message"
 concourse_msg_field_value="All tests passed."
 concourse_msg_body="This message is to test the Concourse FlowDrone integration."
 
-echo $(expr ${concourse_msg_threadID} + 1) > .thread_id
-
-
 HEADER="\"flow_token\": \"${concourse_flow_token}\", \"event\": \"${concourse_msg_event}\", \"author\": {\"name\": \"${concourse_display_name}\", \"avatar\": \"${concourse_avatar}\"}"
 MSGINFO="\"title\": \"${concourse_msg_title}\", \"external_thread_id\": \"${concourse_msg_threadID}\""
 MSGTHREAD="\"thread\": {\"title\": \"${concourse_msg_thread_title}\", \"fields\": [{\"label\": \"${concourse_msg_field_label}\", \"value\": \"${concourse_msg_field_value}\"}], \"body\": \"${concourse_msg_body}\", \"external_url\": \"${concourse_pipeline_url}\", \"status\": {\"color\": \"${concourse_msg_status_color}\", \"value\": \"${concourse_msg_status_value}\"}}"
@@ -24,4 +23,16 @@ MSGTHREAD="\"thread\": {\"title\": \"${concourse_msg_thread_title}\", \"fields\"
 MESSAGE="{${HEADER}, ${MSGINFO}, ${MSGTHREAD}}"
 API_URL="https://api.flowdock.com/messages"
 
-./run.sh ${MESSAGE} ${API_URL}
+if [[ -z ${MESSAGE} ]]; then
+  echo -e "Cannot post message to Flowdock. \n Reason: Message is unset or empty."
+  exit 1
+fi
+
+if [[ -z ${API_URL} ]]; then
+  echo -e "Cannot post message to Flowdock. \n Reason: API URL is unset or empty."
+  exit 1
+fi
+
+COMMAND="curl -i -X POST -H \"Content-Type: application/json\" -d '${MESSAGE}' ${API_URL}"
+#echo $COMMAND
+eval $COMMAND
